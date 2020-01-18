b6cad1e8a51a29f0c899bcc0c7e8572c1c8890e2
ElvisDream <290418903@qq.com>
2020-01-18 23:15:43 +0800

    图书馆信息管理系统页面资源加载正确

--- src/main/java/com/holiday/HolidayBooksManagerSystemApplication.java
+++ src/main/java/com/holiday/HolidayBooksManagerSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HolidaybooksmanagersystemApplication {
+public class HolidayBooksManagerSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HolidaybooksmanagersystemApplication.class, args);
+        SpringApplication.run(HolidayBooksManagerSystemApplication.class, args);
     }
 
 }
