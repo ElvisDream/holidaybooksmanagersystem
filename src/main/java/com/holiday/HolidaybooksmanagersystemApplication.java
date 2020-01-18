980e9df79009b450eb7cc376b9088f650291fd1e
ElvisDream <290418903@qq.com>
2020-01-18 20:44:54 +0800

    图书馆信息管理系统

---
+++ src/main/java/com/holiday/HolidaybooksmanagersystemApplication.java
@@ -0,0 +1,13 @@
+package com.holiday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidaybooksmanagersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolidaybooksmanagersystemApplication.class, args);
+    }
+
+}
