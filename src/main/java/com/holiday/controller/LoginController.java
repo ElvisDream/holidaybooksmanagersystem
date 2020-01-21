88b8cca7dad73fea97e0c5fab897835363a52adc
ElvisDream <290418903@qq.com>
2020-01-21 13:43:44 +0800

    完成登录及用户名密码错误提示 完成登录成功跳转到其他界面

--- src/main/java/com/holiday/controller/LoginController.java
+++ src/main/java/com/holiday/controller/LoginController.java
@@ -3,12 +3,10 @@ package com.holiday.controller;
 import com.holiday.entity.User;
 import com.holiday.service.UserService;
 import com.holiday.util.RestResult;
-import com.holiday.util.ResultCode;
 import com.holiday.util.ResultGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,8 +23,7 @@ public class LoginController {
     private ResultGenerator resultGenerator;
 
     @RequestMapping("/login")
-    public String index(HttpServletRequest request) {
-        request.getSession().invalidate();
+    public String index() {
         return "login";
     }
 
@@ -37,10 +34,15 @@ public class LoginController {
         String password = request.getParameter("password");
         User loginUser = userService.findByUserInfo(userName, password);
         if (loginUser == null) {
-            return resultGenerator.getFailResult("用户名未找到或密码不正确！");
+            return resultGenerator.getFailResult("用户名或密码不正确！");
         } else {
             return resultGenerator.getSuccessResult(loginUser);
         }
+    }
+
+    @GetMapping("/success")
+    public String success() {
+        return "success";
     }
 
 }
